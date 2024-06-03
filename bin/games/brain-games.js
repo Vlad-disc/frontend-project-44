b8b5759a49da584ffb693d1438f7beea74f4c84a
Усков Владислав <uskov_vladislav@icloud.com>
2024-06-03 08:51:38 +0300

    eighth lesson

--- bin/games/brain-games.js
+++ bin/games/brain-games.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { greetingsUser } from '../../src/index.js';
 
 greetingsUser();
---
+++ bin/games/brain-progression.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { greetingsUser, getRandomNumOne, getRandomNumTwo } from '../../src/index.js';
+
+const userName = greetingsUser();
+let answerPoint = 0;
+let functionRep = 0;
+
+console.log('What number is missing in the progression?');
+
+const progressionQuestion = () => {
+  if (functionRep < 3) {
+    
+    const randomNumArray = getRandomNumOne();
+    const stepsArray = getRandomNumTwo();
+
+    const progressionArray = [];
+    const lengthArray = 10;
+
+    for (let i = 0; i < stepsArray * lengthArray; i += stepsArray) {
+        progressionArray.push(randomNumArray + i);
+    }
+
+    const randomNum = Math.floor(Math.random() * progressionArray.length);
+    const correctAnswer = progressionArray[randomNum];
+    
+    progressionArray[randomNum] = '..'
+
+    const resultArray = progressionArray.join(' ');
+
+
+    console.log(`Question: ${resultArray}`);
+
+    const answerUser = readlineSync.question('Your answer: ');
+
+    if (Number(answerUser) === correctAnswer) {
+      console.log('Correct!');
+      answerPoint += 1;
+    } else if (Number(answerUser) !== correctAnswer) {
+      return console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}!`);
+    }
+
+    if (answerPoint === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    } else {
+      functionRep += 1;
+      progressionQuestion();
+    }
+  }
+};
+
+progressionQuestion();
